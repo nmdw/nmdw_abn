d167b81dc8438e3713233404c869ac65a6ce3b78
Michiel de Winter <michiel.de.winter@nl.abnamro.com>
2019-03-14 13:31:35 +0100

    first commit

---
+++ infra/data_lake.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+DATA_LAKE_RG='sandbox-nl02330-003-rg'
+DATA_LAKE_LOCATION='westeurope'
+# Create a new Resource Group
+# az group create \
+# --name $DATA_LAKE_RG \
+# --location $DATA_LAKE_LOCATION
+
+
+DATA_LAKE_STORAGE='mdwdatalakestorage'
+# Create a new storage (ADLS Gen2) Account
+# az storage account create \
+# --name $DATA_LAKE_STORAGE  \
+# --resource-group $DATA_LAKE_RG  \
+# --location $DATA_LAKE_LOCATION  \
+# --sku Standard_LRS \
+# --kind StorageV2  \
+# --hierarchical-namespace true
+
+DATA_LAKE_WORKFLOW='datalakeorchestrator'
+ARM_LOCATION='arm/data_factory.json'
+ARM_PROPS_LOCATION='conf/data_factory_prop.json'
+# Create Data Factory Version 2 
+# az group deployment create \
+# --name $DATA_LAKE_WORKFLOW \
+# --resource-group $DATA_LAKE_RG \
+# --template-file $ARM_LOCATION \
+# --parameters $ARM_PROPS_LOCATION
+
+DATA_LAKE_SERVER_NAME='mdw-sql-server'
+DATA_LAKE_DATABASE_NAME='mdw-sql-db'
+ADMIN_USERNAME='mdw'
+ADMIN_PASSWD='Wachtwoord01!'
+## Create a logical server in the resource group
+az sql server create \
+ --name $DATA_LAKE_SERVER_NAME \
+ --resource-group $DATA_LAKE_RG \
+ --location $DATA_LAKE_LOCATION  \
+ --admin-user $ADMIN_USERNAME \
+ --admin-password $ADMIN_PASSWD
+## Create a database in the server 
+az sql db create \
+ --resource-group $DATA_LAKE_RG \
+ --server $DATA_LAKE_SERVER_NAME \
+ --name $DATA_LAKE_DATABASE_NAME \
+ --sample-name AdventureWorksLT \
+ --service-objective S0
+# Configure a firewall rule for the server
+az sql server firewall-rule create \
+ --resource-group $DATA_LAKE_RG \
+ --server $DATA_LAKE_SERVER_NAME \
+ -n AllowYourIp \
+ --start-ip-address 0.0.0.0 \
+ --end-ip-address 0.0.0.0
