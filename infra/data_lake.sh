86d70eb084c54a852bee17a4195c265860c8e1be
Michiel de Winter <michiel.de.winter@nl.abnamro.com>
2019-03-14 14:05:27 +0100

    Fifth commit

--- infra/data_lake.sh
+++ infra/data_lake.sh
@@ -32,34 +32,42 @@ DATA_LAKE_DATABASE_NAME='mdw-sql-db'
 ADMIN_USERNAME='mdw'
 ADMIN_PASSWD='Wachtwoord01!'
 ## Create a logical server in the resource group
-az sql server create \
- --name $DATA_LAKE_SERVER_NAME \
- --resource-group $DATA_LAKE_RG \
- --location $DATA_LAKE_LOCATION  \
- --admin-user $ADMIN_USERNAME \
- --admin-password $ADMIN_PASSWD
-## Create a database in the server 
-az sql db create \
- --resource-group $DATA_LAKE_RG \
- --server $DATA_LAKE_SERVER_NAME \
- --name $DATA_LAKE_DATABASE_NAME \
- --sample-name AdventureWorksLT \
- --service-objective S0
-# Configure a firewall rule for the server
-az sql server firewall-rule create \
- --resource-group $DATA_LAKE_RG \
- --server $DATA_LAKE_SERVER_NAME \
- -n AllowYourIp \
- --start-ip-address 0.0.0.0 \
- --end-ip-address 0.0.0.0
+# az sql server create \
+#  --name $DATA_LAKE_SERVER_NAME \
+#  --resource-group $DATA_LAKE_RG \
+#  --location $DATA_LAKE_LOCATION  \
+#  --admin-user $ADMIN_USERNAME \
+#  --admin-password $ADMIN_PASSWD
+# ## Create a database in the server 
+# az sql db create \
+#  --resource-group $DATA_LAKE_RG \
+#  --server $DATA_LAKE_SERVER_NAME \
+#  --name $DATA_LAKE_DATABASE_NAME \
+#  --sample-name AdventureWorksLT \
+#  --service-objective S0
+# # Configure a firewall rule for the server
+# az sql server firewall-rule create \
+#  --resource-group $DATA_LAKE_RG \
+#  --server $DATA_LAKE_SERVER_NAME \
+#  -n AllowYourIp \
+#  --start-ip-address 0.0.0.0 \
+#  --end-ip-address 0.0.0.0
 
 
 DATA_LAKE_WORKFLOW_DB='mdw-databricks'
 ARM_LOCATION_DB='arm/databricks.json'
 ARM_PROPS_LOCATION_DB='../conf/databricks_prop.json'
 # Create Azure Databricks 
-az group deployment create \
- --name $DATA_LAKE_WORKFLOW_DB \
-        --resource-group $DATA_LAKE_RG \
- --template-file $ARM_LOCATION_DB \
- --parameters $ARM_PROPS_LOCATION_DB
+# az group deployment create \
+#  --name $DATA_LAKE_WORKFLOW_DB \
+#         --resource-group $DATA_LAKE_RG \
+#  --template-file $ARM_LOCATION_DB \
+#  --parameters $ARM_PROPS_LOCATION_DB
+
+
+ DATA_LAKE_DWH_NAME='<ENTER_SQL_DWH_NAME>'
+# Create a SQL Datawarehouse
+az sql dw create \
+ --resource-group $DATA_LAKE_RG \
+ --server $DATA_LAKE_SERVER_NAME \
+ --name $DATA_LAKE_DWH_NAME
